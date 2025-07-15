62c6a8820c7437c5ae8e60f4a323f023817de408
Lula Robertson <lrobertson4@luc.edu>
2025-07-15 15:19:36 +0000

    super and super

--- SuperAles.py
+++ SuperAles.py
@@ -1,4 +1,30 @@
+class Beer:
+    def __init__(self, name:str, abv:int):
+        self.name = name
+        self.abv = abv
 
+    def alcohol_content(self, volume_in_oz):
+        return self.abv*volume_in_oz
+    
+    def description(self):
+        return f"{self.name}: {self.abv*100:.1f}% ABV"
+    
+class Pale_Ale(Beer): # new SUBclass. pale ale is still a beer.
+    def __init__(self):
+        super().__init__("Pale Ale", 0.055)
+         # refering back to "superclass" beer, name is ".." and ABV is an int.
+
+class IPA(Beer):
+    def __init__(self):
+        super().__init__("IPA", 0.065)
+
+class Stout(Beer):
+    def __init__(self):
+        super().__init__("Stout", 0.07)
+
+class Porter(Beer):
+    def __init__(self):
+        super().__init__("Porter", 0.06)
 
 #--------------------------------------------------------------------------------#
 # ⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎  WRITE YOUR CODE ABOVE THIS  LINE ⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎
--- SuperVehicles.py
+++ SuperVehicles.py
@@ -1,4 +1,26 @@
+class Vehicle:
+    def __init__(self, name:str, mpg:int):
+        self.name = name
+        self.mpg = mpg
 
+    def fuel_needed(self, distance):
+        return distance / self.mpg
+    
+    def description(self):
+        return f"{self.name} get {self.mpg} miles per gallon."
+    
+class Truck(Vehicle):
+    def __init__(self):
+        super().__init__("Truck", 15)
+        # dot-operator to refer to the function __init__ from the superclass: Vehicle.
+
+class Motorcycle(Vehicle):
+    def __init__(self):
+        super().__init__("Motorcycle", 50)
+
+class Bus(Vehicle):
+    def __init__(self):
+        super().__init__("Bus", 8)
 
 #--------------------------------------------------------------------------------#
 # ⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎  WRITE YOUR CODE ABOVE THIS  LINE ⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎
